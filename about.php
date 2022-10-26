dd949e4ac8e25177c9364b06f837ba50e7031c9d
Karan2980 <95915381+Karan2980@users.noreply.github.com>
2022-10-27 00:21:52 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+<header class="main">
+        <nav>
+            <a href="#" class="logo">
+                <img src="images/logo1.png"/>
+            </a>
+
+            <ul class="menu">
+                <li><a href="index.php" class="active">Home</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="main-heading">
+            <h1>Create Prespectives With Virtual Reality</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint provident consectetur ducimus. Blanditiis, culpa!</p>
+            <a class="main-btn" href="#">Contact</a>
+        </div>
+    </header>
+        <div class="jumbotron">
+  <h1>Eldas</h1>
+  <p>we rise by lifting you</p>
+</div>
+</body>
+</html>
